041fee637740ec0ac1dc66514baa588b28ed93cd
AkhilaParadi <142782231+AkhilaParadi@users.noreply.github.com>
2024-08-14 01:07:50 +0530

    Add files via upload

---
+++ rough.js
@@ -0,0 +1,7 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.getElementById('mobileNav').style.display = 'flex';
+});
+
+document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('mobileNav').style.display = 'none';
+});
